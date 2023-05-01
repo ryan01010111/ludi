24b6bae1fc71fef1f790892c50db86607abe4b1f
Ryan Williams <ryanwilliamsfb@gmail.com>
2023-05-01 01:37:17 -0500

    fix backend prod image build

--- backend/compose_services.sh
+++ backend/compose_services.sh
@@ -2,8 +2,9 @@
 
 docker_compose_files_args='-f docker-compose.yaml -f docker-compose.development.yaml'
 
-function await_postgres() {
-    local try_count=0;
+await_postgres() {
+    set +e
+    local try_count=0
     while true; do
         if [[ try_count -ge 60 ]]; then
             echo 'Postgres not ready after 1 min'
@@ -11,11 +12,10 @@ function await_postgres() {
         fi
         ((try_count+=1))
         echo "Trying Postgres (${try_count}/60):"
-        set +e
         docker compose exec -T postgres pg_isready && break
-        set -e
         sleep 1
     done
+    set -e
 }
 
 set -e
